a36e04ded52776a34d30a055d4a7b4e74c2fd252
RDIPIPOM <ezamora2@ucol.mx>
2019-11-29 15:19:08 -0600

    2. Terminado

--- Project/MetodoExtraer.js
+++ Project/MetodoExtraer.js
@@ -1,3 +1,17 @@
-function extraer(){
-    
+function extraer() {
+    let aux = this._inicio;
+    while (aux != null && aux.prioridad === 0) {
+        aux = aux.siguiente;
+    }
+
+    if (aux != null) {
+        aux.anterior.siguiente = aux.siguiente;
+        aux.siguiente.anterior = aux.anterior;
+        return aux;
+    } else {
+        let valorARetornar = this._inicio;
+        this._inicio.siguiente.anterior = null;
+        this._inicio = this._inicio.siguiente;
+        return valorARetornar;
+    }
 }
