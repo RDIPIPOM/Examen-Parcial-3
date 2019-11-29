692b02fbb2442e81c874910ae82e571c97ca6c41
RDIPIPOM <ezamora2@ucol.mx>
2019-11-29 15:35:25 -0600

    4. Validando que el inicio tena prioridad 1

--- Project/MetodoExtraer.js
+++ Project/MetodoExtraer.js
@@ -1,25 +1,18 @@
 function extraer() {
     //Consideré una lista doblemente enlazada (no circular)
     let aux = this._inicio;
-    if (this._inicio.prioridad != 1) {
-        while (aux != null && aux.prioridad === 0)
-            aux = aux.siguiente;
+    while (aux != null && aux.prioridad === 0)
+        aux = aux.siguiente;
 
-        if (aux != null) {
-            aux.anterior.siguiente = aux.siguiente;
-            if (aux.siguiente != null)
-                aux.siguiente.anterior = aux.anterior;
-            return aux;
-        } else {
-            aux = this._inicio;
-            this._inicio.siguiente.anterior = null;
-            this._inicio = this._inicio.siguiente;
-            return aux;
-        }
-    } else {
+    if (this._inicio.prioridad === 1 || aux === null) {
         aux = this._inicio;
         this._inicio.siguiente.anterior = null;
         this._inicio = this._inicio.siguiente;
+        return aux;
+    } else {
+        aux.anterior.siguiente = aux.siguiente;
+        if (aux.siguiente != null)
+            aux.siguiente.anterior = aux.anterior;
         return aux;
     }
 }
